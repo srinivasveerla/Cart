fa4ae3cc405ace44dcbafc64167ded32f73c2390
srinivasveerla <vdssrinivas98@gmail.com>
2024-02-03 17:26:30 -0500

    Added event listener to the button

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function logText() {
-  var text = document.getElementById("textbox");
-  console.log(text.value);
-}
+const add_btn = document.getElementById("add-btn");
+const add_item = document.getElementById("add-item");
+
+add_btn.addEventListener("click", (e) => console.log(add_item.value));
